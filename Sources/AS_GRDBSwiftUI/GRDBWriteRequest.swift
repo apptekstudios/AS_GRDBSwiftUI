c51232ebf17f53641ce76195e24776abadcc7285
Apptek Studios <contact@apptekstudios.com>
2020-04-17 15:35:56 +1000

    Some renaming of protocol functions

--- Sources/AS_GRDBSwiftUI/GRDBWriteRequest.swift
+++ Sources/AS_GRDBSwiftUI/GRDBWriteRequest.swift
@@ -6,16 +6,16 @@ import GRDB
 public protocol GRDBWriteRequest
 {
 	associatedtype Result
-	func defineWriteTransaction(db: GRDB.Database) throws -> Result
+	func onWrite(db: GRDB.Database) throws -> Result
 
-	func execute(inDB databaseWriter: DatabaseWriter) throws -> Result
+	func executeRequest(inDB databaseWriter: DatabaseWriter) throws -> Result
 }
 
 public extension GRDBWriteRequest
 {
-	func execute(inDB databaseWriter: DatabaseWriter) throws -> Result {
+	func executeRequest(inDB databaseWriter: DatabaseWriter) throws -> Result {
 		try databaseWriter.write {
-			try defineWriteTransaction(db: $0)
+			try onWrite(db: $0)
 		}
 	}
 }
